fb1b97d6f20a629251129d3129ce09e106d6daba
AHMEDBRZAN <85833696+AHMEDBRZAN@users.noreply.github.com>
2026-09-04 13:22:00 +0300

    Update app_settings.dart

--- lib/core/app_settings.dart
+++ lib/core/app_settings.dart
@@ -5,15 +5,18 @@ class AppSettings extends ChangeNotifier {
   bool _isDark = true;
   bool _isArabic = true;
   bool _isLoggedIn = false;
+  bool _isGuest = false;
 
   bool get isDark => _isDark;
   bool get isArabic => _isArabic;
   bool get isLoggedIn => _isLoggedIn;
+  bool get isGuest => _isGuest;
 
   void toggleDark() { _isDark = !_isDark; notifyListeners(); }
   void toggleLanguage() { _isArabic = !_isArabic; notifyListeners(); }
-  void login() { _isLoggedIn = true; notifyListeners(); }
-  void logout() { _isLoggedIn = false; notifyListeners(); }
+  void login() { _isLoggedIn = true; _isGuest = false; notifyListeners(); }
+  void loginAsGuest() { _isLoggedIn = true; _isGuest = true; notifyListeners(); }
+  void logout() { _isLoggedIn = false; _isGuest = false; notifyListeners(); }
 
   String tr(String key) => Strings.get(_isArabic, key);
 }
