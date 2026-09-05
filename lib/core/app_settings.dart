004eef9260ccf343a75bb9d8eaec86d0dfda96be
AHMEDBRZAN <85833696+AHMEDBRZAN@users.noreply.github.com>
2026-09-05 19:38:00 +0300

    Refactor login methods and user handling

--- lib/core/app_settings.dart
+++ lib/core/app_settings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'store_service.dart';
 import 'strings.dart';
 
 class AppSettings extends ChangeNotifier {
@@ -7,41 +8,38 @@ class AppSettings extends ChangeNotifier {
   bool _isLoggedIn = false;
   bool _isGuest = false;
   bool _isAdmin = false;
+  User? _user;
 
   bool get isDark => _isDark;
   bool get isArabic => _isArabic;
   bool get isLoggedIn => _isLoggedIn;
   bool get isGuest => _isGuest;
   bool get isAdmin => _isAdmin;
+  User? get user => _user;
+  int get points => _user?.points ?? 0;
 
   void toggleDark() { _isDark = !_isDark; notifyListeners(); }
   void toggleLanguage() { _isArabic = !_isArabic; notifyListeners(); }
 
-  void login() {
-    _isLoggedIn = true;
-    _isGuest = false;
-    _isAdmin = false;
-    notifyListeners();
-  }
-
   void loginAsGuest() {
-    _isLoggedIn = true;
-    _isGuest = true;
-    _isAdmin = false;
+    _isLoggedIn = true; _isGuest = true; _isAdmin = false; _user = null;
     notifyListeners();
   }
 
   void loginAsAdmin() {
-    _isLoggedIn = true;
-    _isGuest = false;
-    _isAdmin = true;
+    _isLoggedIn = true; _isGuest = false; _isAdmin = true; _user = null;
+    notifyListeners();
+  }
+
+  void loginAsUser(User u) {
+    _isLoggedIn = true; _isGuest = false;
+    _isAdmin = u.role == 'admin';
+    _user = u;
     notifyListeners();
   }
 
   void logout() {
-    _isLoggedIn = false;
-    _isGuest = false;
-    _isAdmin = false;
+    _isLoggedIn = false; _isGuest = false; _isAdmin = false; _user = null;
     notifyListeners();
   }
 
