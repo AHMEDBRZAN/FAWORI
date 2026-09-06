4d417bbe61b239c116f45c84df76edf22599a84b
AHMEDBRZAN <85833696+AHMEDBRZAN@users.noreply.github.com>
2026-09-06 10:46:32 +0300

    Update app_settings.dart

--- lib/core/app_settings.dart
+++ lib/core/app_settings.dart
@@ -7,14 +7,12 @@ class AppSettings extends ChangeNotifier {
   bool _isArabic = true;
   bool _isLoggedIn = false;
   bool _isGuest = false;
-  bool _isAdmin = false;
   User? _user;
 
   bool get isDark => _isDark;
   bool get isArabic => _isArabic;
   bool get isLoggedIn => _isLoggedIn;
   bool get isGuest => _isGuest;
-  bool get isAdmin => _isAdmin;
   User? get user => _user;
   int get points => _user?.points ?? 0;
 
@@ -22,24 +20,17 @@ class AppSettings extends ChangeNotifier {
   void toggleLanguage() { _isArabic = !_isArabic; notifyListeners(); }
 
   void loginAsGuest() {
-    _isLoggedIn = true; _isGuest = true; _isAdmin = false; _user = null;
-    notifyListeners();
-  }
-
-  void loginAsAdmin() {
-    _isLoggedIn = true; _isGuest = false; _isAdmin = true; _user = null;
+    _isLoggedIn = true; _isGuest = true; _user = null;
     notifyListeners();
   }
 
   void loginAsUser(User u) {
-    _isLoggedIn = true; _isGuest = false;
-    _isAdmin = u.role == 'admin';
-    _user = u;
+    _isLoggedIn = true; _isGuest = false; _user = u;
     notifyListeners();
   }
 
   void logout() {
-    _isLoggedIn = false; _isGuest = false; _isAdmin = false; _user = null;
+    _isLoggedIn = false; _isGuest = false; _user = null;
     notifyListeners();
   }
 
