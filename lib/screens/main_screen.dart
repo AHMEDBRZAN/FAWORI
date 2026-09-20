d5bd868d5e9015aee17bb6f22d52a0cf0d34e9ba
AHMEDBRZAN <85833696+AHMEDBRZAN@users.noreply.github.com>
2026-09-20 18:22:49 +0300

    update lib/screens/main_screen.dart

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -30,7 +30,6 @@ class _MainScreenState extends State<MainScreen> {
     ];
 
     return Scaffold(
-      // ✅ تمرير index لعرض الصفحة المختارة
       body: IndexedStack(index: _idx, children: screens),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _idx,
