8fbc9802b43d4ae0c8276ac3f2acfbba95fdc3f7
AHMEDBRZAN <85833696+AHMEDBRZAN@users.noreply.github.com>
2026-09-23 10:53:26 +0300

    update lib/screens/simple_screens.dart

--- lib/screens/simple_screens.dart
+++ lib/screens/simple_screens.dart
@@ -1325,7 +1325,6 @@ class _AdminPointsViewState extends State<AdminPointsView> {
                   onPressed: () => setState(() {
                         _sel = null;
                         _filter = 'all';
-                        _q = '';
                       }),
                 ),
               )
